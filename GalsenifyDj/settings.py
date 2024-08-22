10ea114a57fe547de8d389183b637f577a78d552
sibylassana95 <sibyamara95@gmail.com>
2024-08-22 09:22:23 +0000

    update setting

--- GalsenifyDj/settings.py
+++ GalsenifyDj/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, ''),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 MEDIA_URL = 'images/'
 MEDIA_ROOT = 'images/'
