0792eb9f59d2ea3014f6613c4c1ea0b828668ab6
sibylassana95 <sibyamara95@gmail.com>
2024-08-22 09:08:25 +0000

    update requirement

--- build_files.sh
+++ build_files.sh
@@ -8,7 +8,7 @@ source venv/bin/activate
 
 # install all deps in the venv
 pip install -r requirements.txt
-pip install mysqlclient
+pip install mysqlclient==2.2.4
 pip install django-cors-headers
 # collect static files using the Python interpreter from venv
 python manage.py collectstatic 
