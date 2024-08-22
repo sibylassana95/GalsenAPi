d45601dd30e1560c12343f3226b5b4b8f26338b8
sibylassana95 <sibyamara95@gmail.com>
2024-08-22 08:43:33 +0000

    update setting

--- GalsenifyDj/settings.py
+++ GalsenifyDj/settings.py
@@ -110,13 +110,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 MEDIA_URL = 'images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
-
+MEDIA_ROOT = 'images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
