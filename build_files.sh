c76b8df57fcfdeb8e1b3c8cd90d2389cced9da00
sibylassana95 <sibyamara95@gmail.com>
2024-08-22 10:10:17 +0000

    update build

--- build_files.sh
+++ build_files.sh
@@ -6,6 +6,7 @@ python3.9 -m venv venv
 # activate the virtual environment
 source venv/bin/activate
 pip install --upgrade setuptools
+sudo apt-get install python3-pymysql
 pip install pymysql
 # install all deps in the venv
 pip install -r requirements.txt
