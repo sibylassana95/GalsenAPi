94fea2b894de89ec482287ba6122cd16ce468fcb
sibylassana95 <sibyamara95@gmail.com>
2024-08-24 15:15:30 +0000

    update setting

--- build_files.sh
+++ build_files.sh
@@ -5,15 +5,13 @@ python3.9 -m venv venv
 
 # activate the virtual environment
 source venv/bin/activate
-pip install --upgrade pip
 pip install --upgrade setuptools
+python3.9 -m pip install PyMySQL
 # install all deps in the venv
 pip install -r requirements.txt
-pip install Django==5.0.1
 pip install django-cors-headers
 # collect static files using the Python interpreter from venv
 python3.9 manage.py collectstatic 
-python3.9 -c "import pymysql; print('pymysql is installed')"
 
 echo "BUILD END"
 
