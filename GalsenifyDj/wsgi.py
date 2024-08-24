dda00301bb61da04985b6cacd4f06bb2bcd9bee3
sibylassana95 <sibyamara95@gmail.com>
2024-08-24 13:50:06 +0000

    update setting

--- GalsenifyDj/wsgi.py
+++ GalsenifyDj/wsgi.py
@@ -3,8 +3,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GalsenifyDj.settings')
 
-# Exposez l'application Django WSGI en tant que `app`
-app = get_wsgi_application()
-
-# Pour être sûr que Vercel trouve la variable, vous pouvez aussi la nommer `handler`
-handler = app
+# api/settings.py
+WSGI_APPLICATION = 'api.wsgi.app'
