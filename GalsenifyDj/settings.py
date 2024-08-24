6615d808b5884d615107cfff0e80b4d48dfd43a8
sibylassana95 <sibyamara95@gmail.com>
2024-08-24 13:57:44 +0000

    update setting

--- GalsenifyDj/settings.py
+++ GalsenifyDj/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'GalsenifyDj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
