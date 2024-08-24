31c918c5994c8ddfb8a93edbb1eb6a2b3ba55888
sibylassana95 <sibyamara95@gmail.com>
2024-08-24 13:53:44 +0000

    update setting

--- GalsenifyDj/wsgi.py
+++ GalsenifyDj/wsgi.py
@@ -1,7 +1,6 @@
 import os
+
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GalsenifyDj.settings')
-
-# api/settings.py
-WSGI_APPLICATION = 'api.wsgi.app'
+application = get_wsgi_application()
+app = application
