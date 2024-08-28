d85360e376e5b2df8f0dcfd5ede27db226bbb4ec
sibylassana95 <sibyamara95@gmail.com>
2024-08-28 21:05:15 +0000

    updates setting

--- build_files.sh
+++ build_files.sh
@@ -5,14 +5,18 @@ python3.9 -m venv venv
 
 # activate the virtual environment
 source venv/bin/activate
-pip install --upgrade setuptools
-sudo apt-get install pkg-config python3-dev default-libmysqlclient-dev build-essential
-# install all deps in the venv
+
+# install build dependencies (no need for sudo in this script)
+apt-get update
+apt-get install -y pkg-config python3-dev default-libmysqlclient-dev build-essential
+
+# install all dependencies in the venv
+pip install --upgrade pip setuptools
 pip install -r requirements.txt
 pip install django-cors-headers
 
 # collect static files using the Python interpreter from venv
 rm -rf staticfiles_build/static/
-python3.9 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 echo "BUILD END"
