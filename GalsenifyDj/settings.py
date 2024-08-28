e998fcefec8220a9e247981129b47969b085598b
sibylassana95 <sibyamara95@gmail.com>
2024-08-28 08:39:43 +0000

    update setting

--- GalsenifyDj/settings.py
+++ GalsenifyDj/settings.py
@@ -126,10 +126,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 else:
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 
