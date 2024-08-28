8016640816a567381f79d61948b5f7ea092a7013
sibylassana95 <sibyamara95@gmail.com>
2024-08-28 08:01:13 +0000

    update setting

--- GalsenifyDj/settings.py
+++ GalsenifyDj/settings.py
@@ -125,9 +125,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
-
 MEDIA_URL = 'images/'
-MEDIA_ROOT = 'images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
