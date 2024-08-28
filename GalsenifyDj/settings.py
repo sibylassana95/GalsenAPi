534dedc38c15b728ea031e4a393cdbbd4ee447e6
sibylassana95 <sibyamara95@gmail.com>
2024-08-28 20:36:29 +0000

    update setting

--- GalsenifyDj/settings.py
+++ GalsenifyDj/settings.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from dotenv import find_dotenv, load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,7 +79,7 @@ DATABASES = {
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("PASSWORD"),
-        'HOST': os.getenv("HOST"),   # Or an IP Address that your DB is hosted on
+        'HOST': os.getenv("HOST"),
         'PORT': '3306',
     }
 }
