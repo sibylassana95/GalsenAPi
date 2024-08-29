29b45778dd326e97e3fd27e329d5f0951198bb68
sibylassana95 <sibyamara95@gmail.com>
2024-08-29 00:01:56 +0000

    updates config

--- GalsenifyDj/settings.py
+++ GalsenifyDj/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
