ead209674ebe647a3ffa87364909c8ea51f785ec
sibylassana95 <sibyamara95@gmail.com>
2025-02-01 18:35:12 +0000

    supprime les variables MEDIA_URL et MEDIA_ROOT dans settings.py pour simplifier la configuration des fichiers statiques

--- GalsenifyDj/settings.py
+++ GalsenifyDj/settings.py
@@ -129,12 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-MEDIA_URL = '/images/'
+
 
 STATIC_URL = 'static/'
-STATIC_ROOT = (BASE_DIR/"static/")
-    
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+STATIC_ROOT = (BASE_DIR/"static/")   
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
