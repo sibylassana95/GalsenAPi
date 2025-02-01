4298708743946f2177faf638b7bc245431765bfc
sibylassana95 <sibyamara95@gmail.com>
2025-02-01 19:46:32 +0000

    met à jour les liens vers la documentation pour utiliser le nom d'URL au lieu des chemins statiques

--- GalsenifyDj/urls.py
+++ GalsenifyDj/urls.py
@@ -25,7 +25,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin', admin.site.urls),
     path('', include('app.urls')),
-    path('docs', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs', schema_view.with_ui('swagger', cache_timeout=0), name='docs'),
     path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 handler404="app.views.error_404"
