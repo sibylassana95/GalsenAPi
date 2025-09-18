d4225c4a354138f48a1d8594f894a731ca6d43e0
sibylassana95 <sibyamara95@gmail.com>
2025-09-18 21:11:28 +0000

    Mise a jour

--- GalsenifyDj/settings.py
+++ GalsenifyDj/settings.py
@@ -1,12 +1,16 @@
 import os
 from pathlib import Path
-from urllib.parse import urlparse
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
+import pymysql
+pymysql.install_as_MySQLdb()
+
+ENVIRONMENT = os.getenv('DJANGO_ENV') 
+print(ENVIRONMENT)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-ENVIRONMENT = os.getenv('DJANGO_ENV', 'production')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -83,17 +87,16 @@ if ENVIRONMENT == 'local':
     } 
 
 else:
-    tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': tmpPostgres.path.replace('/', ''),
-        'USER': tmpPostgres.username,
-        'PASSWORD': tmpPostgres.password,
-        'HOST': tmpPostgres.hostname,
-        'PORT': 5432,
-    }
-}
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('DB_PROD_NAME'),
+            'USER': os.getenv('DB_PROD_USER'),
+            'PASSWORD': os.getenv('DB_PROD_PASSWORD'),
+            'HOST': os.getenv('DB_PROD_HOST'),
+            'PORT': os.getenv('DB_PROD_PORT'),
+        }
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -116,24 +119,32 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Dakar'
 
 USE_I18N = True
 
+USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-MEDIA_URL = '/images/'
-
-STATIC_URL = 'static/'
-STATIC_ROOT = (BASE_DIR/"static/")
-    
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+if ENVIRONMENT == 'local':
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    MEDIA_URL = '/images/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+else:
+    MEDIA_URL = '/images/'
+    STATIC_URL = 'static/'
+    STATIC_ROOT = '/home/mivo8940/galsenapi.lassanasiby.com/static'
+    MEDIA_ROOT = '/home/mivo8940/galsenapi.lassanasiby.com/images'
 
 
 
