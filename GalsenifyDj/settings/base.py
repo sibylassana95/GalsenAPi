a9a00fc63d357474ba8adaafc69340e120ab094e
sibylassana95 <sibyamara95@gmail.com>
2026-08-23 08:18:19 +0000

    feat(frontend): design system institutional modern - tailwind compile, layout global et recherche instantanee

--- GalsenifyDj/settings/base.py
+++ GalsenifyDj/settings/base.py
@@ -61,6 +61,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'GalsenifyDj.context_processors.site_stats',
             ],
         },
     },
